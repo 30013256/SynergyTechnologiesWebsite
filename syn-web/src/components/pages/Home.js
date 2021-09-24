a8f5c0581508c367168bd2cfdad80ac8d4e53d45
unknown <jacquesrockell@gmail.com>
2021-09-24 14:39:06 +1200

    updates

--- syn-web/src/components/pages/Home.js
+++ syn-web/src/components/pages/Home.js
@@ -3,12 +3,13 @@ import Hero from '../layout/Hero';
 import Header from "../layout/Header";
 
 import WAVES from 'vanta/dist/vanta.waves.min';
-import { Link } from 'react-router-dom';
+import { Link, useHistory as history} from 'react-router-dom';
 
-import SwiperCore, { Navigation } from 'swiper';
+import SwiperCore, { Navigation, Mousewheel } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import { Pagination } from 'swiper';
+import { HashNavigation } from 'swiper';
 
 import Logo from "../images/synlogo.png";
 import Clouds from '../images/Clouds.jpg';
@@ -17,10 +18,12 @@ import TechMap from '../images/techroadmap.jpg';
 import Laptop from '../images/laptop.jpg';
 import Continuity from '../images/continuity.jpg';
 import Support from '../images/support.jpg';
+import Team from '../images/team.png';
+import Community from '../images/community.jpg'
 
 import { Squash as Hamburger } from 'hamburger-react'
 
-SwiperCore.use([Pagination, Navigation]);
+SwiperCore.use([Pagination, Navigation, Mousewheel, HashNavigation]);
 
 export class Home extends Component {
     constructor() {
@@ -40,7 +43,7 @@ export class Home extends Component {
             scaleMobile: 1.00,
             shininess: 28.00,
             waveHeight: 8.00,
-            waveSpeed: .5,
+            waveSpeed: .8,
             color: 0x0,
         })
         
@@ -59,12 +62,21 @@ export class Home extends Component {
         this.setState({isOpen: !this.state.isOpen})
     }
 
+    toggleContact(){
+        this.setState({FormOpen: !this.state.FormOpen})
+    }
+
+    navigate(){
+
+    }
+
     state = {
+        FormOpen: false,
         CardExpanded: false,
         isOpen: false,
         cardNum: 0,
         infoCardTitle: [
-            "YOUR CLOUD STRATERGY",
+            "YOUR CLOUD STRATEGY",
             "Technology To Enable Your Organisation",
             "TECHNOLOGY ROAD MAP",
             "Procurement",
@@ -95,6 +107,51 @@ export class Home extends Component {
                         <p>{this.state.infoCardContent[this.state.cardNum]}</p>
                     </div>
                 </div>
+                <div className={this.state.FormOpen ? "contact-form form-open" : "contact-form"}>
+                    <div onClick = {() => this.toggleContact()} className="click-mask"></div>
+                    <form>
+                        <h1>Contact Us</h1>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="var(--dark-grey)" class="bi bi-person" viewBox="0 0 16 16">
+                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                            </svg>
+                            <input placeholder="Name"></input>
+                        </div>  
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="var(--dark-grey)" class="bi bi-envelope" viewBox="0 0 16 16">
+                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+                            </svg>
+                            <input placeholder="Email"></input>
+                        </div> 
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="var(--dark-grey)" class="bi bi-telephone" viewBox="0 0 16 16">
+                                <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                            </svg>
+                            <input placeholder="Phone"></input>
+                        </div>                      
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="var(--dark-grey)" class="bi bi-people" viewBox="0 0 16 16">
+                                <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+                            </svg>
+                            <input placeholder="Company"></input>
+                        </div>  
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="var(--dark-grey)" class="bi bi-journal-text" viewBox="0 0 16 16">
+                                <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                                <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+                                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+                            </svg>
+                            <select placeholder="Enquiry">
+                                <option value="" disabled selected>Select Enquiry</option>
+                                <option>General</option>
+                                <option>Ticket</option>
+                                <option>Jobs</option>
+                            </select>
+                        </div>
+                        <textarea placeholder="Message"></textarea> 
+                        <button>SEND</button>                                 
+                    </form>
+                </div>
                 <header id={"headerId"} className={this.props.scroll ? "bg" : ""}>
                     <nav>
                         <div class="logo">
@@ -111,29 +168,29 @@ export class Home extends Component {
                 <div className={this.state.isOpen ? "menu menu-open" : "menu"}>
                     <ul>
                         <li><Link to="">About Us</Link></li>
-                        <li><Link to="">Contact Us</Link></li>
-                        <li><Link to="">Community</Link></li>
-                        <li><Link to="">Services</Link></li>
+                        <li><Link to="" >Contact</Link></li>
+                        <li><Link to="" >Services</Link></li>
+                        <li><Link to="" >Careers</Link></li>
                         <li><p>synergyemail@synergy.com | 022 011 211 | 25 Somthing St</p></li>
                     </ul>
                 </div>
                 <div id="Hero" ref={this.vantaRef} >               
-                    <Swiper className="mySwiper" allowTouchMove={false} pagination={true} pagination={{"clickable": true}} navigation={true}>
-                        <SwiperSlide>
+                    <Swiper className="mySwiper" hashNavigation={{"watchState": true}} mousewheel={true} allowTouchMove={false} pagination={true} pagination={{"clickable": true}} navigation={true}>
+                        <SwiperSlide data-hash="home">
                             <h1>EXTEND AND ENABLE YOUR IT TEAM</h1>
                             <p>Already have an IT Team in place, but want to leverage our capacity and experience? We’ll work along side your organisation to reach your objectives faster with our flexible approach.</p>
                             <div>
-                                <Link to="" className={"btn-contact"}>Contact</Link>
+                                <Link to="" onClick = {(e) => this.toggleContact()} className={"btn-contact"}>Contact</Link>
                                 <Link to="" className={"btn-customerportal"}>Customer Portal</Link>
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide>
-                            <h1>SERVICES</h1>
+                        <SwiperSlide data-hash="services">
+                            <h1>SERVICES</h1>                           
                             <div className={"services"}>
                                 <div className={"service-card-v2"} onClick = {(e) => this.cardClick(0)}>
                                     <img src={Clouds} />
                                     <div id={"sc-div-id-2"}>
-                                        <h1 id={"sc-header-id-1"}>Your Cloud Stratergy</h1>
+                                        <h1 id={"sc-header-id-1"}>Your Cloud Strategy</h1>
                                         <p>
                                             Already have an IT Team in place, but want to leverage our capacity and experience?
                                         </p>
@@ -192,9 +249,34 @@ export class Home extends Component {
                                 </div>                        
                             </div>                        
                         </SwiperSlide>
-                        <SwiperSlide>
-                            <h1>Community/Other Important content</h1>
-                            <p>Commodo nostrud laborum esse cupidatat excepteur quis sit Lorem do veniam amet magna. Ut do duis culpa excepteur ipsum deserunt aliquip. Proident minim deserunt consequat dolor.</p>
+                        <SwiperSlide data-hash="aboutus">
+                            <h1>About Us</h1>
+                            <div className={"about-us"}>
+                                <div className={"flip-card"}>                           
+                                    <div className={"flip-card-inner"}>
+                                        <div className={"flip-card-front"}>
+                                            <img src={Team}/>
+                                            <h1>Our Team</h1>
+                                        </div>
+                                        <div className={"flip-card-back"}>
+                                            <h1>Our Team</h1>
+                                            <p>Cillum et cillum elit veniam. Aute officia quis nulla tempor dolore exercitation incididunt aliqua excepteur eu labore magna. Voluptate deserunt ex fugiat amet ad cupidatat. Labore Lorem sint exercitation do nisi anim laborum. </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={"flip-card"}>
+                                    <div className={"flip-card-inner"}>
+                                        <div className={"flip-card-front"}>
+                                            <img src={Community}/>
+                                            <h1>Our Community</h1>
+                                        </div>
+                                        <div className={"flip-card-back yellow-bg"}>
+                                            <h1>Our Community</h1>
+                                            <p>Cillum et cillum elit veniam. Aute officia quis nulla tempor dolore exercitation incididunt aliqua excepteur eu labore magna. Voluptate deserunt ex fugiat amet ad cupidatat. Labore Lorem sint exercitation do nisi anim laborum. </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </SwiperSlide>
                     </Swiper>         
                 </div>
